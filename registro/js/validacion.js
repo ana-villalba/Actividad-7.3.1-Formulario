6e1c87c619ea37ee79624b2af5e85e02104cabc4
ana-villalba <analaura.villalba23@gmail.com>
2024-08-19 20:52:51 -0300

    Update validacion.js

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,8 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+if(pasword.length <6) {
+    errorMessage.textContent = "La contraseña debe tener al menos 6 caracteres.";
+    return;
+}
